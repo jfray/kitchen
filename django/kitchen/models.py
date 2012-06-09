456b6a0f691c5d4058a316850c9dd752ef474898
Josh Fraser <josh@threefoldphotos.com>
2012-06-08 23:49:13 -0700

    * creating a flask directory for an experiment

--- django/kitchen/models.py
+++ django/kitchen/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from djchoices import DjangoChoices, ChoiceItem
 from durationfield.db.models.fields.duration import DurationField
 
-class Member(models.Model):
+class Person(models.Model):
   first_name = models.CharField(max_length=50)
   last_name = models.CharField(max_length=50)
   email = models.EmailField()
@@ -11,21 +11,21 @@ class Member(models.Model):
     return " ".join([self.first_name, self.last_name])
 
 class Customer(models.Model):
-  member = models.ForeignKey(Member)
+  member = models.ForeignKey(Person)
   credits = models.IntegerField()
   
   def __unicode__(self):
     return "%s:%s" % (self.member, self.credits)
 
 class Line(models.Model):
-  member = models.ForeignKey(Member)
+  member = models.ForeignKey(Person)
   is_available = models.BooleanField()
 
   def __unicode__(self):
     return "%s:Available" % self.member if self.is_available else "%s:Unavailable" % self.member
 
 class Party(models.Model):
-  member = models.ForeignKey(Member)
+  member = models.ForeignKey(Person)
   is_notified = models.BooleanField()
   is_approver = models.BooleanField()
 
@@ -35,7 +35,7 @@ class Party(models.Model):
     return "%s:%s,%s" % (self.member, n, a)
 
 class Expediter(models.Model):
-  member = models.ForeignKey(Member)
+  member = models.ForeignKey(Person)
   is_notified = models.BooleanField()
 
   def __unicode__(self):
